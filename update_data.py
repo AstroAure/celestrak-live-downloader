9c7fe432f9788617c981a898d0bbfc01e1d04550
Aurélien Genin <aurelien.genin@cube-monde.net>
2026-07-04 18:42:18 +0200

    Add auto download scripts

---
+++ update_data.py
@@ -0,0 +1,53 @@
+import requests
+import pandas as pd
+import os
+import json
+from datetime import datetime
+
+sat_ids = os.getenv('SAT_INTDES_IDS', '').split(',')
+
+os.makedirs("data", exist_ok=True)
+
+def download_json(id):
+	"""Download latests orbital data for given sat id"""
+	url = f"https://celestrak.org/NORAD/elements/gp.php?INTDES={id}&FORMAT=JSON"
+	response = requests.get(url)
+	response.raise_for_status()
+	return response.json()[0]
+
+def update_csv(id, new_data):
+	"""Updates CSV database for given sat id"""
+	csv_path = f"data/{id}.csv"
+	# Loads database (if it exists)
+	if os.path.exists(csv_path):
+		df_existing = pd.read_csv(csv_path)
+	else:
+		df_existing = pd.DataFrame()
+		df_existing.insert(0, 'SAVED_AT', None)
+		df_existing.insert(1, 'EPOCH', None)
+
+	if data['EPOCH'] not in df_existing['EPOCH'].values:
+		# Adds a new row and timestamp
+		new_row = {**new_data, 'SAVED_AT': datetime.now().isoformat()}
+		df_new = pd.DataFrame([new_row])
+		df_updated = pd.concat([df_existing, df_new], ignore_index=True)
+		df_updated.to_csv(csv_path, index=False)
+		print(f"New line added to database for satellite {id}")
+		return True
+	else:
+		print(f"No new update for satellite {id}")
+		return False
+
+if __name__ == "__main__":
+	if not SAT_INTDES_IDS or SAT_INTDES_IDS == ['']:
+		raise ValueError("No satellite ids defined in SAT_INTDES_IDS")
+	for id in SAT_INTDES_IDS:
+		id = id.strip()
+		if not id:
+			continue
+		try:
+			data = download_json(id)
+			update_csv(id, data)
+		except Exception as e:
+			print(f"Error for satellite {id}: {e}")
+			continue
